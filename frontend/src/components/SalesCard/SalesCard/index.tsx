ba1520f3466b53bf0669740e488a7f978d5ea6d6
gzbrdr <gabrielzbardini@gmail.com>
2022-07-15 20:35:10 -0300

    Sale listing

--- frontend/src/components/SalesCard/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/SalesCard/index.tsx
@@ -1,11 +1,13 @@
 import axios from "axios"
 
 import { useEffect, useState } from "react"
-import SalesTableRow from "../SalesTableRow"
+import NotificationButton from "../NotificationButton"
 import SalesTableHead from "../SalesTableHead"
 import ReactDatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import "./styles.css"
+import { BASE_URL } from "../../../utils/request"
+import { Sale } from "../../../models/sale"
 
 
 function SalesCard() {
@@ -14,13 +16,16 @@ function SalesCard() {
     const [minDate, setMinDate] = useState(min)
     const [maxDate, setMaxDate] = useState(max)
 
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
-        axios.get("http://localhost:8080/sales")
+        axios.get(`${BASE_URL}/sales`)
             .then(response => {
-                console.log(response.data);
+                setSales(response.data.content)
             });
     }, [])
 
+    
     return (
     <>
     <div className="card">
@@ -47,9 +52,25 @@ function SalesCard() {
             <table className="sales-table">
             <SalesTableHead />
             <tbody>
-                <SalesTableRow />
-                <SalesTableRow />
-                <SalesTableRow />
+                {
+                    sales.map(sale => {
+                        return (
+                            <tr key={sale.id}>
+                                <td className="show992">#{sale.id}</td>
+                                <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                <td>{sale.sellerName}</td>
+                                <td className="show992">{sale.visited}</td>
+                                <td className="show992">{sale.deals}</td>
+                                <td>{sale.amount.toFixed(2)}</td>
+                                <td>
+                                    <div className="red-btn-container">
+                                        <NotificationButton />
+                                    </div>
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
             </tbody>
             </table>
         </div>
