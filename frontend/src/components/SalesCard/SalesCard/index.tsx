5be164d80ae5addfbfd4db554cccdef73fbe26ca
gzbrdr <gabrielzbardini@gmail.com>
2022-07-15 22:15:49 -0300

    Date Updating

--- frontend/src/components/SalesCard/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/SalesCard/index.tsx
@@ -19,11 +19,13 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content)
             });
-    }, [])
+    }, [minDate, maxDate])
 
     
     return (
@@ -61,7 +63,7 @@ function SalesCard() {
                                 <td>{sale.sellerName}</td>
                                 <td className="show992">{sale.visited}</td>
                                 <td className="show992">{sale.deals}</td>
-                                <td>{sale.amount.toFixed(2)}</td>
+                                <td>R$ {sale.amount.toFixed(2)}</td>
                                 <td>
                                     <div className="red-btn-container">
                                         <NotificationButton />
