fd7e85d92cacd2f58039b28c5542f13b2facd3ec
gzbrdr <gabrielzbardini@gmail.com>
2022-07-15 22:27:46 -0300

    SMS on click

--- frontend/src/components/SalesCard/NotificationButton/index.tsx
+++ frontend/src/components/SalesCard/NotificationButton/index.tsx
@@ -1,11 +1,21 @@
+import axios from "axios";
 import notifIcon from "../../../assets/img/notification-icon.svg"
 import "./styles.css"
+import { BASE_URL } from "../../../utils/request"
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+}
+
+function NotificationButton( {saleId} : Props ) {
     return (
         <>
         <div className="red-btn">
-            <a href="#">
+            <a onClick={() => handleClick(saleId)}>
                 <i><img src={notifIcon} alt="Notificar"/></i>
             </a>
         </div>
--- frontend/src/components/SalesCard/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/SalesCard/index.tsx
@@ -66,7 +66,7 @@ function SalesCard() {
                                 <td>R$ {sale.amount.toFixed(2)}</td>
                                 <td>
                                     <div className="red-btn-container">
-                                        <NotificationButton />
+                                        <NotificationButton saleId={sale.id}/>
                                     </div>
                                 </td>
                             </tr>
